068374951289b85522e3cf3130acdb46a70d2d6b
arshiashirzad <sirzad383@gmail.com>
2024-03-18 00:27:47 +0330

    coding migrations and connecting app to database

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+namespace ExpenseTracker.Models;
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions options):base(options)
+    {}
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ExpenseTracker.Models;
+
+public class Category
+{
+    [Key] 
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required.")]
+    public string Title { get; set; } = "";
+    [Column(TypeName ="nvarchar(255)")]
+    public string Icon { get; set; } = "";
+    [Column(TypeName ="nvarchar(255)")]
+    public string Type { get; set; } = "Expense";
+    // [NotMapped]
+    // public string? TitleWithIcon
+    // {
+    //     get
+    //     {
+    //         return this.Icon + " " + this.Title;
+    //     }
+    // }
+}
